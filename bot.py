689076763464c529eaf046faa05dbd7765a18440
seostar <seostar@i.ua>
2026-01-31 23:03:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,6 @@ def scan_and_update():
 
     for u in updates:
         if not u.message or str(u.message.chat.id) != str(CHAT_ID): continue
-        
         msg_time = datetime.fromtimestamp(u.message.date, pytz.utc).astimezone(TIMEZONE)
         if (now - msg_time).total_seconds() > 86400: continue
 
@@ -51,7 +50,6 @@ def scan_and_update():
                     target_months.append(m_idx)
             
             if not target_months: target_months = [now.month]
-
             for num in found_numbers:
                 if num in active_apps:
                     for m_idx in target_months:
@@ -75,22 +73,24 @@ def run_logic():
     ukr_months = {1:"січень", 2:"лютий", 3:"березень", 4:"квітень", 5:"травень", 6:"червень", 
                   7:"липень", 8:"серпень", 9:"вересень", 10:"жовтень", 11:"листопад", 12:"грудень"}
     m_name = ukr_months[now.month]
+    
+    signature = "\n\n_🤖 Бот навчається: якщо я помилився, просто напишіть номер вашої кв._"
 
     if os.getenv('GITHUB_EVENT_NAME') == 'workflow_dispatch':
-        bot.send_message(CHAT_ID, config['templates'][now.month-1].format(month_name=m_name, neighbors_list=", ".join(active_list), card=config['card_details'], amount=config['monthly_fee']), message_thread_id=THREAD_ID, parse_mode='Markdown')
-        bot.send_message(CHAT_ID, random.choice(config['report_templates']).format(month_name=m_name, paid_list=", ".join(sorted(paid, key=int)) if paid else "нікого", unpaid_list=", ".join(unpaid) if unpaid else "всіх!"), message_thread_id=THREAD_ID, parse_mode='Markdown')
+        bot.send_message(CHAT_ID, config['templates'][now.month-1].format(month_name=m_name, neighbors_list=", ".join(active_list), card=config['card_details'], amount=config['monthly_fee']) + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
+        bot.send_message(CHAT_ID, random.choice(config['report_templates']).format(month_name=m_name, paid_list=", ".join(sorted(paid, key=int)) if paid else "нікого", unpaid_list=", ".join(unpaid) if unpaid else "всіх!") + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
         if unpaid:
-            bot.send_message(CHAT_ID, random.choice(config['reminder_templates']).format(month_name=m_name, unpaid_list=", ".join(unpaid), card=config['card_details']), message_thread_id=THREAD_ID, parse_mode='Markdown')
+            bot.send_message(CHAT_ID, random.choice(config['reminder_templates']).format(month_name=m_name, unpaid_list=", ".join(unpaid), card=config['card_details']) + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
         return
 
     day, hour = now.day, now.hour
     if day == 1 and hour == 9:
-        bot.send_message(CHAT_ID, config['templates'][now.month-1].format(month_name=m_name, neighbors_list=", ".join(active_list), card=config['card_details'], amount=config['monthly_fee']), message_thread_id=THREAD_ID, parse_mode='Markdown')
+        bot.send_message(CHAT_ID, config['templates'][now.month-1].format(month_name=m_name, neighbors_list=", ".join(active_list), card=config['card_details'], amount=config['monthly_fee']) + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
     elif day == 11 and hour == 12:
-        bot.send_message(CHAT_ID, random.choice(config['report_templates']).format(month_name=m_name, paid_list=", ".join(sorted(paid, key=int)) if paid else "нікого", unpaid_list=", ".join(unpaid) if unpaid else "всіх!"), message_thread_id=THREAD_ID, parse_mode='Markdown')
+        bot.send_message(CHAT_ID, random.choice(config['report_templates']).format(month_name=m_name, paid_list=", ".join(sorted(paid, key=int)) if paid else "нікого", unpaid_list=", ".join(unpaid) if unpaid else "всіх!") + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
     elif day == 19 and hour == 12:
         if unpaid:
-            bot.send_message(CHAT_ID, random.choice(config['reminder_templates']).format(month_name=m_name, unpaid_list=", ".join(unpaid), card=config['card_details']), message_thread_id=THREAD_ID, parse_mode='Markdown')
+            bot.send_message(CHAT_ID, random.choice(config['reminder_templates']).format(month_name=m_name, unpaid_list=", ".join(unpaid), card=config['card_details']) + signature, message_thread_id=THREAD_ID, parse_mode='Markdown')
 
 if __name__ == "__main__":
     run_logic()
